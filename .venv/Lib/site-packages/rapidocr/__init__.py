171a5e1cbfa72947315a124323d6ae5e855113a2
William <william@example.com>
2025-07-08 01:28:59 +0800

    remove .venv form git tracking

--- .venv/Lib/site-packages/rapidocr/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-# @Author: SWHL
-# @Contact: liekkaskono@163.com
-from .main import RapidOCR
-from .utils import LoadImageError, VisRes
-from .utils.typings import EngineType, LangCls, LangDet, LangRec, ModelType, OCRVersion
